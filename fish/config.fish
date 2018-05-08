8dfef021c562d6eb735e6767e0623e925ed671f1
Cauê Baasch de Souza <cauebs@pm.me>
2018-05-08 01:59:43 -0300

    ...

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,8 @@
-alias ls=exa
+alias ls exa
+alias find fd
+alias grep rg
+alias time hyperfine
+alias cp "cp -vr"
+alias mkdir "mkdir -p"
+alias activate "pipenv shell --fancy"
+alias pyg pygmentize
