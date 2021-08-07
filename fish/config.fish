a8704b560184d8b93ac35f5f71fcd5961320f267
Cauê Baasch de Souza <cauebs@pm.me>
2021-08-07 05:10:52 -0300

    ...

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,7 @@ alias start "sudo systemctl start"
 alias stop "sudo systemctl stop"
 alias restart "sudo systemctl restart"
 alias cd.. "cd .."
-alias tectonic nextonic
 
 set XDG_CURRENT_DESKTOP sway
 
-theme_gruvbox dark
+#theme_gruvbox dark
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
     if not set -q __fish_git_prompt_show_informative_status
         set -g __fish_git_prompt_show_informative_status 1
@@ -11,31 +12,7 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_color_branch magenta --bold
     end
     if not set -q __fish_git_prompt_showupstream
-        set -g __fish_git_prompt_showupstream "informative"
-    end
-    if not set -q __fish_git_prompt_char_upstream_ahead
-        set -g __fish_git_prompt_char_upstream_ahead "↑"
-    end
-    if not set -q __fish_git_prompt_char_upstream_behind
-        set -g __fish_git_prompt_char_upstream_behind "↓"
-    end
-    if not set -q __fish_git_prompt_char_upstream_prefix
-        set -g __fish_git_prompt_char_upstream_prefix ""
-    end
-    if not set -q __fish_git_prompt_char_stagedstate
-        set -g __fish_git_prompt_char_stagedstate "●"
-    end
-    if not set -q __fish_git_prompt_char_dirtystate
-        set -g __fish_git_prompt_char_dirtystate "✚"
-    end
-    if not set -q __fish_git_prompt_char_untrackedfiles
-        set -g __fish_git_prompt_char_untrackedfiles "…"
-    end
-    if not set -q __fish_git_prompt_char_invalidstate
-        set -g __fish_git_prompt_char_invalidstate "✖"
-    end
-    if not set -q __fish_git_prompt_char_cleanstate
-        set -g __fish_git_prompt_char_cleanstate "✔"
+        set -g __fish_git_prompt_showupstream informative
     end
     if not set -q __fish_git_prompt_color_dirtystate
         set -g __fish_git_prompt_color_dirtystate blue
@@ -54,19 +31,17 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     set -l color_cwd
-    set -l prefix
     set -l suffix
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        else
             set color_cwd $fish_color_cwd
-            set suffix '$'
+        end
+        set suffix '#'
+    else
+        set color_cwd $fish_color_cwd
+        set suffix '$'
     end
 
     # PWD
@@ -76,8 +51,10 @@ function fish_prompt --description 'Write out the prompt'
 
     printf '%s ' (fish_vcs_prompt)
 
-    set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
-    echo -n $pipestatus_string
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    echo -n $prompt_status
     set_color normal
 
     echo -n "$suffix "
