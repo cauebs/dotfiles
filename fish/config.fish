18dd5e11484f515534fd8bffa46bc3ee7f166143
Cauê Baasch de Souza <cauebs@pm.me>
2023-01-17 19:53:55 -0300

    ...

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,1 @@
-alias ls exa
-alias start "sudo systemctl start"
-alias stop "sudo systemctl stop"
-alias restart "sudo systemctl restart"
-alias cd.. "cd .."
 
-set XDG_CURRENT_DESKTOP sway
-
-#theme_gruvbox dark
-
-alias kabum "curl -s 'https://ssw.inf.br/2/ssw_SSWDetalhado' -X POST --data-raw 'id=tJGo3dKl2ekLyjyQfzla1W2KFG8ou6nwK%2Bg&md=xsB
-%2BJp9ZD1F0V0zcvm7HUg&w=x' > bla.html; lynx -dump bla.html | tail -n +29 | head -n -15 | rg --passthru '^[\d/:\s]*\$'"
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm'
+return {
+    font = wezterm.font 'JetBrains Mono',
+    font_size = 9,
+    color_scheme = 'Dracula',
+}
